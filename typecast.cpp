9a5b9bae235975ccc085693cd3ec05fa731945d2
Jasim Ansari <itsjasim.ansari@gmail.com>
2024-09-24 13:12:46 +0530

    cpp

---
+++ typecast.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // char grade = 'A';
+    // int value = grade;
+    // cout << value << endl;      //65
+
+    // double price = 1545.9455;
+    // int newPrice = (int)price;     
+    // cout << newPrice << endl;          //1545
+
+
+    
+    return 0;
+}
---
+++ variable.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int age = 27;
+    // cout << sizeof(age) << endl;        //4
+
+    // char grade = 'A';
+    // cout << grade << endl;              //A
+
+    // float age = 27;
+    // cout << sizeof(age) << endl;           //4
+
+
+    double age = 27;
+    cout << sizeof(age) << endl;                //8
+    return 0;
+}
