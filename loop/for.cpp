0df9f5d38a00622a98bfcee734c221300bba8113
Jasim Ansari <itsjasim.ansari@gmail.com>
2024-09-30 23:46:59 +0530

    cpp

---
+++ loop/for.cpp
@@ -0,0 +1,94 @@
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int n = 5;
+//     for (int i=1; i<=n; i++){
+//         cout << i <<" ";
+//     }
+//     return 0;
+// }
+
+// print table of 2
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int n = 10;
+//     for (int i=1; i<=n; i++){
+//         cout << i*2 <<" ";
+//     }
+//     return 0;
+// }
+
+
+
+// print table of given number
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int n ;
+//     cout<<"enter the number: ";
+//     cin>>n;
+//     for (int i=1; i<=10; i++){
+//         cout << i*n <<" ";
+//     }
+//     return 0;
+// }
+
+
+// //sum of number from 1 to n
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     cout<<"enter the number: ";
+//     cin>>n;
+//     int sum = 0;
+//     for (int i =1; i<=n;i++){
+//         sum +=i;
+//     }
+//     cout <<"sum = "<<sum<<endl;
+//     return 0;
+// }
+
+
+//sum of all odd number from 1 to n
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     cout<<"enter the number: ";
+//     cin>>n;
+//     int oddSum = 0;
+//     for (int i =1; i<=n;i++){
+//         if(i%2!=0){
+//             oddSum+=i;
+//         }
+//     }
+//     cout <<"oddSum = "<<oddSum<<endl;
+//     return 0;
+// }
+
+
+//sum of all even number from 1 to N
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     cout<<"enter the number: ";
+//     cin>>n;
+//     int oddSum = 0;
+//     for (int i =1; i<=n;i++){
+//         if(i%2 == 0){
+//             oddSum+=i;
+//         }
+//     }
+//     cout <<"oddSum = "<<oddSum<<endl;
+//     return 0;
+// }
+
+
+
+
+
+
+
---
+++ loop/while.cpp
@@ -0,0 +1,66 @@
+//print number 1 to 5
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int count = 1;
+//     while(count <= 5){
+//         cout<< count <<" ";
+//         count++;
+//     }
+//     cout<<endl;
+//     return 0;
+// }
+
+//print table 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int count = 1;
+//     while(count <= 10){
+//         cout<< count*2 <<" ";
+//         count++;
+//     }
+//     cout<<endl;
+//     return 0;
+// }
+
+//sum of number from 1 to n
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     int i=1;
+//     cout<<"enter the number: ";
+//     cin>>n;
+//     int sum = 0;
+//     while(i<=n){
+//         sum +=i;
+//         i++;
+//     }
+//     cout <<"sum = "<<sum<<endl;
+//     return 0;
+// }
+
+
+
+// sum of odd number from 1 to n
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     int i=1;
+//     cout<<"enter the number: ";
+//     cin>>n;
+//     int sum = 0;
+//     while(i<=n){
+//         if(i%2!=0){
+//             sum += i;
+//         }
+//         i++;
+//     }
+//     cout <<"sum = "<<sum<<endl;
+//     return 0;
+// }
+
+
+
